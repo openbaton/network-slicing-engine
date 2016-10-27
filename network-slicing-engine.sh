8e540129a438457d9956a7726158068c4f794e92
Michael Pauls <michael.pauls@fokus.fraunhofer.de>
2016-10-27 13:46:23 +0200

    Imp: script

--- network-slicing-engine.sh
+++ network-slicing-engine.sh
@@ -4,8 +4,8 @@ source ./gradle.properties
 
 _version=${version}
 
-_project_base="/opt/openbaton/network-slicing-engine"
-_process_name="network-slicing-engine"
+_project_base="/opt/openbaton/openbaton-nse"
+_process_name="openbaton-nse"
 _screen_name="openbaton"
 _config_file="/etc/openbaton/nse.properties"
 
@@ -48,7 +48,7 @@ function init {
     if [ ! -f $_config_file ]; then
         if [ $EUID != 0 ]; then
             echo "creating the directory and copying the file"
-            sudo -E sh -c "mkdir /etc/openbaton; cp ${_project_base}/src/main/resources/application.properties ${_config_file}"
+            sudo -E sh -c "mkdir /etc/openbaton; cp ${_project_base}/etc/nse.properties ${_config_file}"
             #echo "copying the file, insert the administrator password" | sudo -kS cp ${_nubomedia_paas_base}/src/main/resources/paas.properties ${_nubomedia_config_file}
         else
             echo "creating the directory"
@@ -65,9 +65,9 @@ function start {
     start_checks
     screen_exists=$(screen -ls | grep ${_screen_name} | wc -l);
     if [ "${screen_exists}" -eq 0 ]; then
-        screen -c screenrc -d -m -S ${_screen_name} -t ${_process_name} java -jar "$_project_base/build/libs/$_process_name-$_version.jar" --spring.config.location=file:${_config_file}
+        screen -c screenrc -d -m -S ${_screen_name} -t network-slicing java -jar "$_project_base/build/libs/$_process_name-$_version.jar" --spring.config.location=file:${_config_file}
     else
-        screen -S $_screen_name -p 0 -X screen -t $_process_name java -jar "$_project_base/build/libs/$_process_name-$_version.jar" --spring.config.location=file:${_config_file}
+        screen -S $_screen_name -p 0 -X screen -t network-slicing java -jar "$_project_base/build/libs/$_process_name-$_version.jar" --spring.config.location=file:${_config_file}
     fi
 }
 
